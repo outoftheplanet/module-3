c0418ce589d7aa8767de118283b3d6eeae4b0cb9
outoftheplanet <168271723+outoftheplanet@users.noreply.github.com>
2024-05-15 13:20:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    function displayMenu(menuType, menuItems) {
+        $('#menu-content').html(`
+            <h2>${menuType} Menu</h2>
+            <p>Here are some delicious ${menuType.toLowerCase()} dishes:</p>
+            <ul>
+                ${menuItems.map(item => <li>${item}</li>).join('')}
+            </ul>
+        `).fadeIn();
+    }
+
+    $('#chicken').click(function() {
+        displayMenu('Chicken', ['Grilled Chicken', 'Chicken Alfredo', 'Chicken Curry']);
+    });
+
+    $('#beef').click(function() {
+        displayMenu('Beef', ['Steak', 'Beef Tacos', 'Beef Stroganoff']);
+    });
+
+    $('#sushi').click(function() {
+        displayMenu('Sushi', ['California Roll', 'Spicy Tuna Roll', 'Dragon Roll']);
+    });
+});
